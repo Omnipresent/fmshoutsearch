cd415ca2e9b7afcf1621bd485e32e64790a1e5e7
Bhaarat Sharma <bhaarat.s@gmail.com>
2011-01-05 00:54:13 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fmshoutsearch::Application.config.secret_token = '072a562815ea7d1f2c877aaef27b98229f4316c71865a5f75108730c586b8e453554f8ff5c181059b7be18f4878b36b1bcd4101a1f13a3cb7e78535ce2f6c6e9'
